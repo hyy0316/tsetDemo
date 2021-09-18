18eaeace2ff1e3306ea8ab7e7ec2255f72d4cbd7
yyhuang <690448224@qq.com>
2021-09-19 02:13:42 +0800

    push master

---
+++ src/main/java/master/master.java
@@ -0,0 +1,11 @@
+package master;
+
+/**
+ * @description: master
+ * Created by yyhuang on 2021/09/19
+ */
+public class master {
+    public static void main(String[] args) {
+        System.out.println("这是master分支上的数据");
+    }
+}
